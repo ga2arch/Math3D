14b67330fd11edac99d827d3797559d6389695e5
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-10-21 21:38:01 +0200

    added conjugate function

--- Functions.h
+++ Functions.h
@@ -17,6 +17,7 @@
 
 #include "Vector.h"
 #include "Matrix.h"
+#include "Quaternion.h"
 #include "Utils.h"
 
 template <size_t N>
@@ -26,13 +27,6 @@ Vector<N> normalize(const Vector<N>& v) {
     return Vector<N>(_mm_mul_ps(r, v.data));
 }
 
-template <size_t N>
-__m128 dot(const Vector<N>& v1, const Vector<N>& v2) {
-    const auto m = _mm_mul_ps(v1.data, v2.data);
-    const auto sum = _mm_hadd_ps(m, m);
-    return _mm_hadd_ps(sum, sum);
-}
-
 __m128 dot(const __m128& p, const __m128& q) {
     const auto m = _mm_mul_ps(p, q);
     const auto sum = _mm_hadd_ps(m, m);
@@ -40,18 +34,8 @@ __m128 dot(const __m128& p, const __m128& q) {
 }
 
 template <size_t N>
-__m128 cross(const Vector<N>& v1, const Vector<N>& v2) {
-    auto sh1 = _mm_shuffle_ps(v1.data, v1.data, _MM_SHUFFLE(0,3,1,2));
-    auto sh2 = _mm_shuffle_ps(v2.data, v2.data, _MM_SHUFFLE(0,2,3,1));
-    
-    const auto mul1 = _mm_mul_ps(sh1, sh2);
-    
-    sh1 = _mm_shuffle_ps(v2.data, v2.data, _MM_SHUFFLE(0,3,1,2));
-    sh2 = _mm_shuffle_ps(v1.data, v1.data, _MM_SHUFFLE(0,2,3,1));
-    
-    const auto mul2 = _mm_mul_ps(sh1, sh2);
-    
-    return _mm_sub_ps(mul1, mul2);
+__m128 dot(const Vector<N>& v1, const Vector<N>& v2) {
+    return dot(v1.data, v2.data);
 }
 
 __m128 cross(const __m128& p, const __m128& q) {
@@ -69,6 +53,11 @@ __m128 cross(const __m128& p, const __m128& q) {
 }
 
 template <size_t N>
+__m128 cross(const Vector<N>& v1, const Vector<N>& v2) {
+    return cross(v1.data, v2.data);
+}
+
+template <size_t N>
 inline __m128 abs_cross(const Vector<N>& v1, const Vector<N>& v2) {
     return _mm_abs_ps(cross(v1, v2));
 }
@@ -81,7 +70,9 @@ Vector<N> operator*(const float s, Vector<N>& v) {
 }
 
 template <size_t N>
-inline Vector<N> lerp(const Vector<N>& v1, const Vector<N>& v2, const float b) {
+inline Vector<N> lerp(const Vector<N>& v1,
+                      const Vector<N>& v2,
+                      const float b) {
     return (1-b)*v1 + b*v2;
 }
 
@@ -102,6 +93,35 @@ Vector<C> operator*(const Vector<R>& v, const Matrix<R,C>& m) {
     result = _mm_add_ps(result, m_row4);
     
     return Vector<C>(result);
+}
+
+Quaternion operator*(const Quaternion& p, const Quaternion& q) {
+    const auto ps = _mm_replicate_w_ps(p.data);
+    const auto qs = _mm_replicate_w_ps(q.data);
+    
+    const auto s1 = _mm_mul_ps(ps, q.data);
+    const auto s2 = _mm_mul_ps(qs, p.data);
+    
+    const auto pvXqv = cross(p.data, q.data);
+    
+    const auto psqs = _mm_mul_ps(ps, qs);
+    const auto pvqv = dot(p.data, q.data);
+    
+    auto r = _mm_add_ps(s1, s2);
+    r = _mm_add_ps(r, pvXqv);
+    
+    auto a = _mm_sub_ps(psqs, pvqv);
+    return Quaternion(Vec3(r), a);
+};
+
+Quaternion conjugate(const Quaternion& q) {
+    __declspec(align(16)) float t[4] = {
+        1, -1, -1, -1
+    };
+    auto a = _mm_load_ps(t);
+    auto r = _mm_mul_ps(a, q.data);
+    
+    return Quaternion(r);
 }
 
 #endif
--- Quaternion.h
+++ Quaternion.h
@@ -21,6 +21,7 @@ class Quaternion {
     
 public:
     Quaternion(): data(data_) {};
+    Quaternion(__m128& m): data_(m), data(data_) {};
     Quaternion(Vec3 v, float angle): data(data_) {
         __declspec(align(16)) float t[4] = {angle/2.0f};
         auto ___a = _mm_load_ps(t);
@@ -45,25 +46,5 @@ private:
     __m128 data_;
     
 };
-
-Quaternion operator*(const Quaternion& p, const Quaternion& q) {
-    const auto ps = _mm_replicate_w_ps(p.data);
-    const auto qs = _mm_replicate_w_ps(q.data);
-    
-    const auto s1 = _mm_mul_ps(ps, q.data);
-    const auto s2 = _mm_mul_ps(qs, p.data);
-    
-    const auto pvXqv = cross(p.data, q.data);
-    
-    const auto psqs = _mm_mul_ps(ps, qs);
-    const auto pvqv = dot(p.data, q.data);
-    
-    auto r = _mm_add_ps(s1, s2);
-    r = _mm_add_ps(r, pvXqv);
-    
-    auto a = _mm_sub_ps(psqs, pvqv);
-    return Quaternion(Vec3(r), a);
-};
-
 
 #endif
