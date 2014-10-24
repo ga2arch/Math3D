815da29b2960a5648de7d80db3df378755933b6a
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-10-24 23:07:25 +0200

    added catch framework and one test example

---
+++ Test/VectorTest.h
@@ -0,0 +1,32 @@
+//
+//  VectorTest.h
+//  3DMath
+//
+//  Created by Gabriele on 24/10/14.
+//  Copyright (c) 2014 Ga2 & co. All rights reserved.
+//
+
+#ifndef _DMath_VectorTest_h
+#define _DMath_VectorTest_h
+
+#include "Vector.h"
+
+SCENARIO( "Vectors", "[vector]" ) {
+    
+    GIVEN( "Vec1" ) {
+        __declspec(align(16)) float v1data[4];
+        __declspec(align(16)) float t[4] = {1.0f};
+        
+        Vec1 v1(1.0f);
+        _mm_storer_ps(v1data, v1.data);
+        
+        WHEN( "The content is checked" ) {
+            for (int i=0; i < 4; i++) {
+                REQUIRE (t[i] == v1data[i]);
+                
+            }
+        }
+    }
+}
+
+#endif
---
+++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3DMath
+//
+//  Created by Gabriele on 24/10/14.
+//  Copyright (c) 2014 Ga2 & co. All rights reserved.
+//
+
+#include <iostream>
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include <immintrin.h>
+
+#include "VectorTest.h"
