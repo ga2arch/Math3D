5558a88b3f69456f34d25ddfcc2eebe712a00e03
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-10-25 15:54:30 +0200

    fixed identation

--- Quaternion.h
+++ Quaternion.h
@@ -35,8 +35,10 @@ public:
     Matrix<3,3> to_matrix() {
         
         auto s1 = _mm_add_ps(data, data); // 2x 2y 2z 2w
-        auto t1 = _mm_shuffle_ps(s1, s1, _MM_SHUFFLE(2,3,3,0)); // 2y 2x 2x w
         
+        //First Row
+        
+        auto t1 = _mm_shuffle_ps(s1, s1, _MM_SHUFFLE(2,3,3,0)); // 2y 2x 2x w
         auto t2 = _mm_shuffle_ps(data, data, _MM_SHUFFLE(2,2,1,0)); // y y z w
         t2 = _mm_reset_ps(t2, 0); // y y z 0
         
@@ -44,7 +46,6 @@ public:
         t3 = _mm_mul_ps(t3, _mm_set_ps(-1.0f,1.0f,1.0f,1.0f)); // -2y^2 2xy 2xz 0
         
         auto t4 = _mm_shuffle_ps(s1, s1, _MM_SHUFFLE(1,1,2,0)); // 2z 2z 2y w
-        
         auto t5 = _mm_shuffle_ps(data, data, _MM_SHUFFLE(1,0,0,0)); // z w w w
         t5 = _mm_reset_ps(t5, 0); // z z w 0
         
@@ -54,8 +55,8 @@ public:
         auto v1 = _mm_add_ps(_mm_set_ps(1.0f,0.0f,0.0f,0.0f), t3);
         v1 = _mm_add_ps(v1, t6);
         
-        //debug(v1);
-        
+        // Second Row
+
         t1 = _mm_shuffle_ps(s1, s1, _MM_SHUFFLE(3,3,2,0)); // 2x 2x 2y w
         
         t2 = _mm_shuffle_ps(data, data, _MM_SHUFFLE(2,3,1,0)); // y x z w
@@ -75,7 +76,7 @@ public:
         auto v2 = _mm_add_ps(_mm_set_ps(0.0f,1.0f,0.0f,0.0f), t3);
         v2 = _mm_add_ps(v2, t6);
         
-        //debug(v2);
+        //Third Row
         
         t1 = _mm_shuffle_ps(s1, s1, _MM_SHUFFLE(3,2,3,0)); // 2x 2y 2x w
         
@@ -90,20 +91,13 @@ public:
         t5 = _mm_shuffle_ps(data, data, _MM_SHUFFLE(0,0,2,0)); // w w y w
         t5 = _mm_reset_ps(t5, 0); // w w y 0
         
-        
         t6 = _mm_mul_ps(t4, t5);
         t6 = _mm_mul_ps(t6, _mm_set_ps(1.0f,-1.0f,-1.0f,1.0f)); // 2yw - 2xw - 2y^2
-        
-        //debug(t3);
-        
         
         auto v3 = _mm_add_ps(_mm_set_ps(0.0f,0.0f,1.0f,0.0f), t3);
         v3 = _mm_add_ps(v3, t6);
         
-        //debug(v3);
-        
         return Matrix<3,3>(Vec3(v1),Vec3(v2),Vec3(v3));
-        
     }
 
     const __m128& data;
