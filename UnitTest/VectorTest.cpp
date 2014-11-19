26fc86eef91426d992d1328299accf3ebc600665
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-19 16:46:47 +0100

    added sub

--- UnitTest/VectorTest.cpp
+++ UnitTest/VectorTest.cpp
@@ -20,6 +20,38 @@ TEST(VectorTest, Creation) {
     }
 }
 
+TEST(VectorTest, Sub) {
+    
+    Vector<4> v1(1.0f,2.0f,5.0f,6.0f);
+    Vector<4> v2(2.0f,3.0f,4.0f,7.0f);
+    
+    auto v3 = v1 - v2;
+    
+    __declspec(align(16)) float t[4];
+    _mm_storer_ps(t, v3.data);
+    
+    ASSERT_EQ(t[0], 1.0f - 2.0f);
+    ASSERT_EQ(t[1], 2.0f - 3.0f);
+    ASSERT_EQ(t[2], 5.0f - 4.0f);
+    ASSERT_EQ(t[3], 6.0f - 7.0f);
+}
+
+TEST(VectorTest, Mul) {
+    
+    Vector<4> v1(1.0f,2.0f,5.0f,6.0f);
+    Vector<4> v2(2.0f,3.0f,4.0f,7.0f);
+    
+    auto v3 = v1 * v2;
+    
+    __declspec(align(16)) float t[4];
+    _mm_storer_ps(t, v3.data);
+    
+    ASSERT_EQ(t[0], 2.0f);
+    ASSERT_EQ(t[1], 6.0f);
+    ASSERT_EQ(t[2], 20.0f);
+    ASSERT_EQ(t[3], 42.0f);
+}
+
 TEST(VectorTest, Div) {
     
     Vector<4> v1(1.0f,2.0f,5.0f,6.0f);
